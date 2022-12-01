d4be48d4b0081a01d4b436134d30a04d9f38cc6f
MinHo Kim <minhokim@MinHoui-MacBookPro.local>
2022-12-01 22:52:17 +0900

    Add Script

---
+++ shell_script/expr-match.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+quote="[\"']"
+match="[^\"']*"
+
+while read line
+do
+    href=$(expr "$line" : ".*href=${quote}\(${match}\)${quote}.*")
+    if [ $? -eq 0 ]; then
+        echo $href
+    fi
+done < index.html
