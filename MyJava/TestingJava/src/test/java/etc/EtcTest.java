240afb2cab00a0e9f71edfcd7d27b7e4287d042a
minhokim <dorabn@gmail.com>
2023-06-16 09:57:25 +0900

    MySpring First Commit

--- MyJava/TestingJava/src/test/java/etc/EtcTest.java
+++ MyJava/TestingJava/src/test/java/etc/EtcTest.java
@@ -1,4 +1,11 @@
 package etc;
 
+import org.junit.jupiter.api.Test;
+
 public class EtcTest {
+    @Test
+    public void etcTest() {
+        float watt = 104.799995f;
+        System.out.println(Math.round(watt * 100) / 100.0);
+    }
 }
---
+++ MySpring/bgp/src/main/java/net/bgp/BgpApplication.java
@@ -0,0 +1,13 @@
+package net.bgp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BgpApplication.class, args);
+    }
+
+}
