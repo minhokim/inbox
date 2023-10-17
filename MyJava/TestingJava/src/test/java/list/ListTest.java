8b9f0ce599b8ed1177658999d78dee53a7cdd3af
minhokim <dorabn@gmail.com>
2023-10-17 10:03:34 +0900

    .

--- MyJava/TestingJava/src/test/java/list/ListTest.java
+++ MyJava/TestingJava/src/test/java/list/ListTest.java
@@ -4,10 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -56,41 +53,65 @@ public class ListTest {
     @Test
     public void toMap() {
         List<Pay> list = getList();
+
         Map<String, Integer> map = list.stream().collect( Collectors.toMap(row -> row.getName(), row -> row.getMoney()) );
         System.out.println(map.values());
     }
 
+    @Test
+    public void sortList() {
+        List<Pay> list = getList();
+
+        list.sort(Comparator.comparing(Pay::getName).reversed());
+        list.forEach(l -> System.out.println(l.getName() + " : " + l.getMoney()));
+    }
+
     /**
+     * List to Map
      * 중복된 Key가 존재하는 경우 3번째 인자인 mergeFunction을 사용하여 처리
      * 아래의 경우 두 값을 Sum
      */
     @Test
-    public void toMapMerge() {
+    public void listToMapBySum() {
         List<Pay> list = getList();
-        Map<String, Integer> map = list.stream().collect( Collectors.toMap(row -> row.getName(), row -> row.getMoney(), Integer::sum) );
-        assertThat(map.get("wed")).isEqualTo(2);
-        System.out.println(map.values());
+        Pay fourPay = new Pay();
+        fourPay.setName("wed");
+        fourPay.setMoney(2);
+        list.add(fourPay);
+
+        Map<String, Integer> firstMap = list.stream().collect( Collectors.toMap(row -> row.getName(), row -> row.getMoney(), Integer::sum) );
+        assertThat(firstMap.get("wed")).isEqualTo(3);
+        System.out.println(firstMap.values());
+
+        Map<String, Integer> secondMap = list.stream().collect( Collectors.toMap(Pay::getName, Pay::getMoney, Integer::sum) );
+        assertThat(secondMap.get("wed")).isEqualTo(3);
+
+        System.out.println(secondMap.values());
     }
 
-    @Test
-    public void toMapMerge2() {
-        List<Pay> list = getList();
-        Map<String, Integer> map = list.stream().collect( Collectors.toMap(Pay::getName, Pay::getMoney, Integer::sum) );
-        System.out.println(map.values());
-    }
 
     /**
+     * List to Map
      * 아래의 경우 새로운 값으로 대체
      */
     @Test
-    public void toMapMergeReplace() {
+    public void listToMapReplace() {
         List<Pay> list = getList();
+        Pay fourPay = new Pay();
+        fourPay.setName("wed");
+        fourPay.setMoney(2);
+        list.add(fourPay);
+
         Map<String, Integer> map = list.stream().collect( Collectors.toMap(row -> row.getName(), row -> row.getMoney(), (oldValue, newValue) -> newValue) );
-        assertThat(map.get("wed")).isEqualTo(20);
+        assertThat(map.get("wed")).isEqualTo(2);
     }
 
     private static List<Pay> getList() {
         List<Pay> pays = new ArrayList<>();
+        Pay threePay = new Pay();
+        threePay.setName("wed");
+        threePay.setMoney(1);
+
         Pay onePay = new Pay();
         onePay.setName("mon");
         onePay.setMoney(1);
@@ -99,18 +120,9 @@ public class ListTest {
         twoPay.setName("tue");
         twoPay.setMoney(1);
 
-        Pay threePay = new Pay();
-        threePay.setName("wed");
-        threePay.setMoney(1);
-
-        Pay fourPay = new Pay();
-        fourPay.setName("wed");
-        fourPay.setMoney(1);
-
         pays.add(onePay);
         pays.add(twoPay);
         pays.add(threePay);
-        pays.add(fourPay);
 
         return pays;
     }
