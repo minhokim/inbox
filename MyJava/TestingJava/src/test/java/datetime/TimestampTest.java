9abb065575c5af7377367baa42661ba369b82850
minhokim <dorabn@gmail.com>
2024-01-10 13:54:46 +0900

    ..

--- MyJava/TestingJava/src/test/java/datetime/TimestampTest.java
+++ MyJava/TestingJava/src/test/java/datetime/TimestampTest.java
@@ -3,6 +3,7 @@ package datetime;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
 public class TimestampTest {
 
@@ -10,5 +11,13 @@ public class TimestampTest {
     public void timestampTest() {
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
         System.out.println(timestamp);
+    }
+
+    @Test
+    public void timestamptoStr() {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        String timestampStr = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(timestamp);
+        System.out.println("Timestamp : " + timestamp);
+        System.out.println("TimestampStr : " + timestampStr);
     }
 }
