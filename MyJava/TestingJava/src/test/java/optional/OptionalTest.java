d6fb319f7bc85382b27f6eb22282297f5bbc1f8d
minhokim <dorabn@gmail.com>
2024-02-29 16:02:50 +0900

    ..

---
+++ MyJava/TestingJava/src/test/java/optional/OptionalTest.java
@@ -0,0 +1,30 @@
+package optional;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+public class OptionalTest {
+
+    @Test
+    public void orElse() {
+        String str = "orElse";
+        String nullStr = null;
+        String res = Optional.ofNullable(nullStr).orElse(defaultStr()); //str이 null or not null 둘 다 메소드 호출됨
+        System.out.println(res);
+    }
+
+    @Test
+    public void orElseGet() {
+        String str = "orElseGet";
+        String nullStr = null;
+        String res = Optional.ofNullable(nullStr).orElseGet(() -> defaultStr()); //str이 null일 때 호출됨
+        System.out.println(res);
+    }
+
+    public static String defaultStr() {
+        System.out.println("Call defaultStr");
+        return "default";
+    }
+
+}
