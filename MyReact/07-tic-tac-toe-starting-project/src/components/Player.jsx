687a70415b21b5c8482aeacd81d589c206370444
MinHo Kim <minhokim@MinHoui-MacBookPro.local>
2024-06-30 22:39:06 +0900

    ..

--- MyReact/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ MyReact/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
+
+
+
 export default function Player({ name, symbol }) {
+    const [ isEditing, setIsEditing ] = useState(false);
+
+    function handleEditClick() {        
+        setIsEditing(true);
+    }
+
+    let playerName = <span className='player-name'>{name}</span>;
+
+    if (isEditing) {
+        playerName = <input className='player' type="text" required />;
+    }
+
+
     return (
         <li>
             <span className="palyer">
-                <span className='player-name'>{name}</span>
+                {playerName}                
                 <span className='player-symbol'>{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>Edit</button>
         </li>
     );
 }
