8bcea7d8f08fe40460620426624040f03a2755a3
MinHo Kim <minhokim@MinHoui-MacBookPro.local>
2024-07-09 07:42:45 +0900

    ..

--- MyReact/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ MyReact/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -9,7 +9,7 @@ export default function Player({ name, symbol }) {
         // 이전 상태값에 기반하여 변경하는 경우 상태 업데이트 함수로 새로운 함수를 보내야 한다.   
         // 즉각적인 상태 변경이 되지 않고 스케줄링한다.             
         //setIsEditing(!isEditing);
-        // () 함수를 리액트가 호출하여 자동으로 현재 상태값을 가지게 된다.
+        // () 함수를 리액트가 호출하여 자동으로 현재 상태값을 가지게 된다. 가장 최신버전의 상태
         setIsEditing((editing) => !isEditing);
     }
 
