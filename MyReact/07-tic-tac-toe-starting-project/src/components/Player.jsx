707cd23942abb21dba37c829f3c53681341bba66
minhokim <dorabn@gmail.com>
2024-08-04 09:35:56 +0900

    20240804

--- MyReact/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ MyReact/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,29 +1,34 @@
 import { useState } from 'react';
 
-
-
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+    const [ playerName, setPlayerName ] = useState(initialName);
     const [ isEditing, setIsEditing ] = useState(false);
-
+    
     function handleEditClick() { 
         // 이전 상태값에 기반하여 변경하는 경우 상태 업데이트 함수로 새로운 함수를 보내야 한다.   
         // 즉각적인 상태 변경이 되지 않고 스케줄링한다.             
         //setIsEditing(!isEditing);
         // () 함수를 리액트가 호출하여 자동으로 현재 상태값을 가지게 된다. 가장 최신버전의 상태
-        setIsEditing((editing) => !isEditing);
+        setIsEditing((editing) => !editing);
     }
 
-    let playerName = <span className='player-name'>{name}</span>;
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
 
     if (isEditing) {
-        playerName = <input type="text" required value={name} />;
+        editablePlayerName = (
+            <input type="text" required value={playerName} onChange={handleChange} />
+        );
     }
 
 
     return (
         <li>
             <span className="palyer">
-                {playerName}                
+                {editablePlayerName}                
                 <span className='player-symbol'>{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
