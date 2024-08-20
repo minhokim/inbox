d1bbeee90384138c3edee0fb748f8da1f8db2e5a
minhokim <dorabn@gmail.com>
2024-08-20 21:52:03 +0900

    CSS

--- MyReact/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ MyReact/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
     const [ playerName, setPlayerName ] = useState(initialName);
     const [ isEditing, setIsEditing ] = useState(false);
     
@@ -27,9 +27,8 @@ export default function Player({ initialName, symbol }) {
         );
     }
 
-
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
             <span className="palyer">
                 {editablePlayerName}                
                 <span className='player-symbol'>{symbol}</span>
